b12640713ba47e9e64df6e7bebfd73ed098e8d2b
georg <georgsteinbacher@gmx.net>
2018-03-15 20:42:55 +0100

    init with demo card/recycler

---
+++ community_jump_trainer/app/src/main/java/georg/steinbacher/community_jump_trainer/Exercise.java
@@ -0,0 +1,15 @@
+package georg.steinbacher.community_jump_trainer;
+
+/**
+ * Created by georg on 15.03.18.
+ */
+
+public class Exercise {
+    String mName;
+    String mDescription;
+
+    Exercise(String name, String description) {
+        mName = name;
+        mDescription = description;
+    }
+}
---
+++ community_jump_trainer/app/src/main/java/georg/steinbacher/community_jump_trainer/MainActivity.java
@@ -0,0 +1,61 @@
+package georg.steinbacher.community_jump_trainer;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    return true;
+                case R.id.navigation_dashboard:
+                    return true;
+                case R.id.navigation_notifications:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Bottom Navigation
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //Receicler
+        RecyclerView rv = findViewById(R.id.rv);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+
+        List<Exercise> exerciseList = new ArrayList<>();
+        exerciseList.add(new Exercise("Excercise1", "This is a long description bla as1 asdsdaf"));
+        exerciseList.add(new Exercise("Excercise2", "This is a long description bla as1 asdsdaf"));
+        exerciseList.add(new Exercise("Excercise3", "This is a long description bla as1 asdsdaf"));
+        exerciseList.add(new Exercise("Excercise4", "This is a long description bla as1 asdsdaf"));
+        exerciseList.add(new Exercise("Excercise5", "This is a long description bla as1 asdsdaf"));
+
+        RVAdapter adapter = new RVAdapter(exerciseList);
+        rv.setAdapter(adapter);
+    }
+
+}
