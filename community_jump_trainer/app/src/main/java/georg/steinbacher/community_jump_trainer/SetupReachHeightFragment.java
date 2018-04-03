f85e98989f78dc4b18b90c2edcc29a9ae3988896
georg <georgsteinbacher@gmx.net>
2018-04-03 18:34:05 +0200

    added setReachedHeight in setup

--- community_jump_trainer/app/src/main/java/georg/steinbacher/community_jump_trainer/SetupReachHeightFragment.java
+++ community_jump_trainer/app/src/main/java/georg/steinbacher/community_jump_trainer/SetupReachHeightFragment.java
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
 import georg.steinbacher.community_jump_trainer.core.Configuration;
 
 
-public class SetupReachHeightFragment extends Fragment{
+public class SetupReachHeightFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -23,6 +23,21 @@ public class SetupReachHeightFragment extends Fragment{
                 ? getString(R.string.centimeters_short) : getString(R.string.inches_short);
         reachHeightInput.setHint(getString(R.string.setup_reach_height_input_hint, system));
 
+        reachHeightInput.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus) {
+                    TextInputEditText reachHeightInput = v.findViewById(R.id.setup_reach_height_input);
+                    try {
+                        double parsedValue = Double.parseDouble(reachHeightInput.getText().toString());
+                        Configuration.getInstance().setReachHeight(parsedValue);
+                    } catch (NumberFormatException e) {
+                        //TODO show error
+                        //not able to parse input
+                    }
+                }
+            }
+        });
         return view;
     }
 }
