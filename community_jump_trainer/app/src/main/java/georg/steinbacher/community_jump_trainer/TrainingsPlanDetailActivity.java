f703423992833af08fe74c8ae1a0878d11aef812
georg <georgsteinbacher@gmx.net>
2018-05-09 19:28:46 +0200

    improved detail screen

--- community_jump_trainer/app/src/main/java/georg/steinbacher/community_jump_trainer/TrainingsPlanDetailActivity.java
+++ community_jump_trainer/app/src/main/java/georg/steinbacher/community_jump_trainer/TrainingsPlanDetailActivity.java
@@ -1,14 +1,34 @@
 package georg.steinbacher.community_jump_trainer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import georg.steinbacher.community_jump_trainer.core.TrainingsPlan;
+import georg.steinbacher.community_jump_trainer.util.Factory;
 
 public class TrainingsPlanDetailActivity extends AppCompatActivity {
+    private static final String TAG = "TrainingsPlanDetailActi";
     public static final String TRAININGS_PLAN_ID = "trainings_plan_id";
+
+    private TrainingsPlan mTrainingsPlan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trainings_plan_detail);
+
+        int trainingsPlanId = getIntent().getIntExtra(TRAININGS_PLAN_ID, -1);
+        if(trainingsPlanId == -1) {
+            Log.e(TAG, "onCreate: No trainingsplanId provided");
+            finish();
+        } else {
+            mTrainingsPlan = Factory.createTraingsPlan(trainingsPlanId);
+        }
+
+        TextView txtName = findViewById(R.id.detail_trainings_plan_name);
+        txtName.setText(mTrainingsPlan.getName());
     }
 }
